56ce475ee02f961491da98a578cb7b42641ff4f9
Fabian Benitez <fabianbenitez25@gmail.com>
2022-11-28 14:15:39 -0300

    modif

--- models/db.js
+++ models/db.js
@@ -6,7 +6,9 @@ var pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DB_NAME
+    database: process.env.MYSQL_DB_NAME,
+    port: process.env.MYSQL_PORT,
+    url: process.env.MYSQ_URL
 })
 
 pool.query = util.promisify(pool.query);
